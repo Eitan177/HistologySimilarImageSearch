52a513f5cf72371d289ecfdf0244a938ec84a71c
Eitan177 <Eitan177@gmail.com>
2020-08-10 13:55:17 -0400

    Update download_selenium.py

--- download_selenium.py
+++ download_selenium.py
@@ -1,9 +1,3 @@
-!pip install selenium
-!apt-get update # to update ubuntu to correctly run apt install
-!apt install chromium-chromedriver
-!cp /usr/lib/chromium-browser/chromedriver /usr/bin
-!pip install pytest
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
